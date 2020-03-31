fa1ff2b10c8f89129838ecbaa0c9aaaaacdc081b
MarcelinoRodrigues <marcelino.rodrigues.d.n@gmail.com>
2020-03-30 21:38:05 -0300

    adjusting in functions

--- public/Js/index.js
+++ public/Js/index.js
@@ -1,33 +1,15 @@
 //Pagina Inicial
 //Criador Marcelino Rodrigues
 
-document.getElementById("btnBuscar").onclick = function(){
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.onreadystatechange = function(){
-		if(xmlHttp.readyState === 4 && xmlHttp.status === 200){
-			document.getElementById("retorno").innerHTML = xmlHttp.responseText;
-		}
-	};
-	xmlHttp.open("Get","curso.txt",true);
-	xmlHttp.send();
-}
-
-
-//IFEE DATA ATUAL
-(function(data = new Date()){
-	var dia = data.getDate();
-	var mes = data.getMonth()+1;
-	var ano = data.getFullYear();
-
-	if (dia.toString().length == 1) dia = '0'+dia;
-	if (mes.toString().length == 1) mes = '0'+mes;
-
-	console.log(dia);
-
-	return dia+'/'+mes+'/'+ano;	
-})();
-
-//Função clique do butão
+//se o botão for clicado
 function clickButton(){
-	console.log("boraaa");
-}
+	document.querySelectorAll('input').onkeydown = function(event){
+		if(event.KeyCode == 13){
+			console.log("kk")
+		}
+	}
+};
+//IFEE FUNCTION
+(function(){
+	console.error("IFEE Persistência");
+})();
