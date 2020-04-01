1c22bebcfef1af493e87e1065124140a84628a2c
MarcelinoRodrigues <marcelino.rodrigues.d.n@gmail.com>
2020-04-01 11:19:29 -0300

    validate form

--- public/Js/User.js
+++ public/Js/User.js
@@ -8,4 +8,8 @@ function FValidation(){
     var name      = document.querySelector('#nomeE');
     var data      = document.querySelector('#dataE');
     var descricao = document.querySelector('#descricao');
+
+    if(name == ''){alert('Preencha por favor o campo nome.');}
+    if(data  == ''){alert('Preencha por favor o campo data.');}
+    if(descricao == ''){alert('Preencha por favor o campo descrição.')}
 }
