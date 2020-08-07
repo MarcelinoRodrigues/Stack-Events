873b542cabdb9f35da55ea352c7ecc29b8e74d4b
Marcelino Rodrigues <marcelino.rodrigues.d.n@gmail.com>
2020-08-06 22:00:19 -0300

    add modal in css

--- public/Js/Evento.js
+++ public/Js/Evento.js
@@ -1,39 +1,26 @@
-//Criação Arquivo Marcelino
-//Adicionando IFEE
-(function(){
-    //Usando cifrão para diminuir o codigo
-    const $ = query => document.getElementById(query)
+const $ = query => document.getElementById(query)
+const js = query => document.createElement(query)
 
-    //usando evento de clique do formulario
-    $('send').addEventListener("click",evento =>{
-        const name = $('nomeE').value;
-        const data = $('dataE').value;
-        const descricao = $('descricao');
+//usando evento de clique do formulario
+$('send').addEventListener("click", () => {
+    const name = $('nomeE').value
+    const data = $('dataE').value
+    const descricao = $('descricao').value
 
-        //validando o formulário
-        if(!name || !data || !descricao){
-            alert("Preencha Todos os campos")
-            return false;
-        }
+    if (!name || !data || !descricao) {
+        alert("Preencha Todos os campos")
+        return false;
+    }
 
-        //criação do objeto Evento
-        const Evento = {
-            name,
-            data,
-            descricao
-        }
-
-
-        const test = localStorage.test ? JSON.parse(localStorage.test) : [];
-        test.push(Evento);
-
-        localStorage.test = JSON.stringify(test);
-
-        //resetando os campos para vazio
-        $('nomeE').value = "";
-        $('dataE').value = "";
-        $('descricao').value = "";
-
-        console.log(test)
-    })
-})()
+    //criação do objeto Evento
+    const Evento = {
+        name,
+        data,
+        descricao
+    }
+    
+    //resetando os campos para vazio
+    $('nomeE').value = "";
+    $('dataE').value = "";
+    $('descricao').value = "";
+})
--- public/Js/index.js
+++ public/Js/index.js
@@ -1,11 +1,3 @@
 //Pagina Inicial
 //Criador Marcelino Rodrigues
 
-//se o botão for clicado
-function clickButton(){
-	document.querySelectorAll('input').onkeydown = function(event){
-		if(event.KeyCode === 13){
-			console.log("kk")
-		}
-	}
-};
